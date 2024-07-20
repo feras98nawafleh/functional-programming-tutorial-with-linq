ef22c7e2c5da4ee77aa5635fb01d4e9da878295e
ferasnawafleh <feras.nawafleh@intalio.com>
2024-07-21 01:20:35 +0400

    Procedural Way

---
+++ FunctionalProgramming/ExtensionProcedural.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class ExtensionProcedural
+    {
+        public static IEnumerable<Employee> GetEmployeesWithFirstNameStartsWith(string value)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if(employee.FirstName.ToLowerInvariant().StartsWith(value.ToLowerInvariant()))
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static IEnumerable<Employee> GetEmployeesWithFirstNameEndsWith(string value)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if (employee.FirstName.ToLowerInvariant().EndsWith(value.ToLowerInvariant()))
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static IEnumerable<Employee> GetEmployeesWithLastNameStartsWith(string value)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if (employee.LastName.ToLowerInvariant().StartsWith(value.ToLowerInvariant()))
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static IEnumerable<Employee> GetEmployeesWithLastNameEndsWith(string value)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if (employee.LastName.ToLowerInvariant().StartsWith(value.ToLowerInvariant()))
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static IEnumerable<Employee> GetEmployeesByYearHired(int yearHired)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if (employee.HireDate.Year == yearHired)
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static IEnumerable<Employee> GetEmployeesByGender(string gender)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if (employee.Gender.ToLower() == gender.ToLower())
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static IEnumerable<Employee> GetEmployeesByPensionPlanValue(bool pensionPlan)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if (employee.HasPensionPlan == pensionPlan)
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static IEnumerable<Employee> GetEmployeesWithSalaryEqualTo(decimal salary)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if (employee.Salary == salary)
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static IEnumerable<Employee> GetEmployeesWithSalaryGreaterThan(decimal value)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if (employee.Salary > value)
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static IEnumerable<Employee> GetEmployeesWithSalaryLessThan(decimal value)
+        {
+            var employees = Repository.GetEmployees();
+            foreach (var employee in employees)
+            {
+                if (employee.Salary < value)
+                {
+                    yield return employee;
+                }
+            }
+        }
+        public static void Print<T>(IEnumerable<T> source, string title)
+        {
+            if (source == null)
+                return;
+            Console.WriteLine();
+            Console.WriteLine("┌───────────────────────────────────────────────────────┐");
+            Console.WriteLine($"│   {title.PadRight(52, ' ')}│");
+            Console.WriteLine("└───────────────────────────────────────────────────────┘");
+            Console.WriteLine();
+            foreach (var item in source)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ FunctionalProgramming/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FunctionalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Procedural Way
+            var q1 = ExtensionProcedural.GetEmployeesWithFirstNameStartsWith("f");
+            ExtensionProcedural.Print(q1, "Employees with First Name Starts With F");
+
+            var q2 = ExtensionProcedural.GetEmployeesWithFirstNameEndsWith("s");
+            ExtensionProcedural.Print(q2, "Employees with Last Name Ends With S");
+
+            var q3 = ExtensionProcedural.GetEmployeesByGender("Male");
+            ExtensionProcedural.Print(q3, "Male Employees");
+
+            var q4 = ExtensionProcedural.GetEmployeesByPensionPlanValue(false);
+            ExtensionProcedural.Print(q4, "Employees with Pension Plan Value set to False");
+        }
+    }
+}
