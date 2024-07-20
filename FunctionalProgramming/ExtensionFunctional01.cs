b647fff6a278f3b51084f6c73769b38653e10236
ferasnawafleh <feras.nawafleh@intalio.com>
2024-07-21 01:51:00 +0400

    Functional Way
    using delegates, Func and Predicate + Extension Methods

---
+++ FunctionalProgramming/ExtensionFunctional01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public static class ExtensionFunctional01
+    {
+        // other way for method signature
+        // public static IEnumerable<Employee> GetEmployeesWithFirstNameStartsWith(IEnumerable<Employee> employees, Func<Employee, bool> predicate)
+
+        public static IEnumerable<Employee> Filter(IEnumerable<Employee> employees, Predicate<Employee> predicate)
+        {
+            foreach (var employee in employees)
+            {
+                if (predicate(employee))
+                {
+                    yield return employee;
+                }
+            }
+        }
+
+        public static void Print<T>(IEnumerable<T> source, string title)
+        {
+            if (source == null)
+                return;
+            Console.WriteLine();
+            Console.WriteLine("┌───────────────────────────────────────────────────────┐");
+            Console.WriteLine($"│   {title.PadRight(52, ' ')}│");
+            Console.WriteLine("└───────────────────────────────────────────────────────┘");
+            Console.WriteLine();
+            foreach (var item in source)
+                Console.WriteLine(item);
+        }
+
+    }
+}
--- FunctionalProgramming/Program.cs
+++ FunctionalProgramming/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            RunProceduralWay();
+            RunFunctionalWay();
+            RunFunctionalWithExtension();
+        }
+
+        public static void RunProceduralWay()
+        {
             // Procedural Way
             var q1 = ExtensionProcedural.GetEmployeesWithFirstNameStartsWith("f");
             ExtensionProcedural.Print(q1, "Employees with First Name Starts With F");
@@ -17,5 +24,38 @@
             var q4 = ExtensionProcedural.GetEmployeesByPensionPlanValue(false);
             ExtensionProcedural.Print(q4, "Employees with Pension Plan Value set to False");
         }
+    
+        public static void RunFunctionalWay()
+        {
+            // Functional way
+
+            var employees = Repository.GetEmployees();
+
+            // another way of passing the predicate:
+            Predicate<Employee> predicate = e => e.LastName.ToLowerInvariant().EndsWith("S");
+            var q0 = ExtensionFunctional01.Filter(employees, predicate);
+            ExtensionFunctional01.Print(q0, "LastName.EndsWith(\"s\")");
+
+            var q1 = ExtensionFunctional01.Filter(employees, e => e.FirstName.ToLower().StartsWith("f"));
+            ExtensionFunctional01.Print(q1, "FirstName.StartsWith(\"f\")");
+
+
+            var q2 = ExtensionFunctional01.Filter(employees, e => e.Salary == 10000);
+            ExtensionFunctional01.Print(q2, "Salary == 10000");
+
+            var q3 = ExtensionFunctional01.Filter(employees, e => e.Salary < 10000);
+            ExtensionFunctional01.Print(q3, "Salary < 10000");
+
+            var q4 = ExtensionFunctional01.Filter(employees, e => e.Salary >= 350000 && e.Gender.ToLowerInvariant() == "female");
+            ExtensionFunctional01.Print(q4, "Salary > 300000 && Gender == \"female\"");
+        }
+
+        public static void RunFunctionalWithExtension()
+        {
+            var employees = Repository.GetEmployees();
+            var q1 = employees.Filter(e => e.Gender.ToLowerInvariant() == "male");
+            q1.Print("Male Employees");
+        }
+
     }
 }
