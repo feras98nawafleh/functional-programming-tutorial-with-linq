578a7fa707e8376ff91f4c3fd85233c309c65642
ferasnawafleh <feras.nawafleh@intalio.com>
2024-07-21 21:48:23 +0400

    video #4

---
+++ Tutorial4/Program.cs
@@ -0,0 +1,89 @@
+﻿using Tutorial4.Shared;
+
+namespace Tutorial4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunExample1();
+            SelectMany();
+            Select();
+            Zipping();
+            LoadLength();
+        }
+
+        private static void RunExample1()
+        {
+            int[] numbersArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<string> words = new() { "I", "Love", ".Net" };
+
+            var resultArray = numbersArray.Where(n => n > 5).Select(n => n * n);
+            var result = words.Select(w => w.ToUpper());
+
+            resultArray.Print("numbers");
+            result.Print("words");
+        }
+
+        public static void Select()
+        {
+            var employees = Repository.GetEmployees();
+
+            var result = 
+                employees
+                .Select(e =>
+                    new EmployeeDTO
+                    {
+                        Name = e.FullName,
+                        TotalSkills = e.Skills.Count()
+                    }
+                );
+            result.Print("list");
+        }
+
+        public static void SelectMany()
+        {
+            var employees = Repository.GetEmployees();
+
+            var query1 =
+                employees
+                .SelectMany(employee => employee.Skills, (employee, skill) => new { employee.FirstName, skill })
+                .Where(e => e.skill.Count() > 0 && e.FirstName.ToLower().StartsWith("f"))
+                .Select(emp => 
+                    new { employeeName = emp.FirstName, Skill = emp.skill }
+                );
+
+            query1.Print("skills");
+        }
+
+        public static void Zipping()
+        {
+
+            // stops until one of the 2 arrays finishes if they're not the same length
+            string[] colorName = { "Red", "Green", "Blue" };
+            string[] colorHex = { "#FF000", "#00FF00", "#0000FF" };
+
+            var colors = colorName.Zip(colorHex, (name, hex) => $"{name} {hex}");
+
+            foreach (var item in colors)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+
+        public static void LoadLength()
+        {
+            var employees = Repository.GetEmployees().ToArray();
+
+            var firstThree = employees[..3];
+            var lastThree = employees[^3..];
+
+            var teams = firstThree.Zip(lastThree, (first, last) => $"{first.FullName} with {last.FullName}");
+
+            var limit = employees.Take(3).ToArray();
+
+            teams.Print("zipped");
+            limit.Print("limited");
+        }
+    }
+}
---
+++ Tutorial4/Shared/EmployeeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial4.Shared
+{
+    public class EmployeeDTO
+    {
+        public string? Name { get; set; }
+        public int TotalSkills{ get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} with Total Skills {TotalSkills}";
+        }
+    }
+}
